3ffc2f31c97c3163bf37f9e40b3995bbed9a1e5b
Кислов Константин Александрович <kostik_kislov@list.ru>
2026-03-09 14:50:41 +0300

    Create create_databases.sql

---
+++ postgres/create_databases.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE fastapi_project_db;
+CREATE DATABASE fastapi_project_test_db;
+GRANT ALL PRIVILEGES ON DATABASE fastapi_project_db TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE fastapi_project_test_db TO postgres;
